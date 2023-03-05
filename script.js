300a3d811c504f29e0825b0bcdcb6a6d69a266b8
Prathmesh Kale <connect.prathmesh905@gmail.com>
2023-03-05 19:08:24 +0530

    pieces moved out

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
 console.log("Hi Mom!")
+function coloring() {
+    const color = document.querySelectorAll('.box')
+
+    color.forEach(color => {
+
+        getId = color.id
+        arr = Array.from(getId)
+        arr.shift()
+        aside = eval(arr.pop())
+        aup = eval(arr.shift())
+        a = aside + aup
+
+        if (a % 2 == 0) {
+            color.style.backgroundColor = '#C3CDDC'
+        }
+        if (a % 2 !== 0) {
+            color.style.backgroundColor = '#8F9EB8 '
+        }
+
+    })
+}
+coloring()
+
+function insertImage() {
+
+    document.querySelectorAll('.box').forEach(image => {
+
+        if (image.innerText.length !== 0) {
+            if (image.innerText == 'Wpawn' || image.innerText == 'Bpawn') {
+                image.innerHTML = `${image.innerText} <img class='allimg allpawn' src="pieces/${image.innerText}.png" alt="">`
+                image.style.cursor = 'pointer'
+
+            }
+
+            else {
+
+                image.innerHTML = `${image.innerText} <img class='allimg' src="pieces/${image.innerText}.png" alt="">`
+                image.style.cursor = 'pointer'
+            }
+        }
+    })
+}
+insertImage()
