77dfe613742461189f64e5379c0a23ccf398598e
Prathmesh Kale <connect.prathmesh905@gmail.com>
2024-04-07 18:25:44 +0530

    fixing ui in 2 player chess

--- bugs.js
+++ bugs.js
@@ -1,11 +1,11 @@
 //! Bugs
 
-//  Timeout function recheck.
-//  Game stop after timeout (cant play moves after X).
-//  Player time stop on turn if opponent out of time.
-//! FIXED  Board flip stop button dosent turn time
+//! Timeout function recheck.
+//! Game stop after timeout (cant play moves after X).
+//! Player time stop on turn if opponent out of time.
+//! --FIXED--  Board flip stop button dosent turn time.
 
 //TODO New features
 
-//? DONE  To make a Stop boardflip button
+// ? --DONE--  To make a Stop boardflip button
 //  
