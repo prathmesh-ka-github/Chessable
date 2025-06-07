4d31e499f382168833f05f5b8940d19eaccef2ab
prathmesh-ka-github <connect.prathmesh905@gmail.com>
2025-06-07 11:05:59 -0400

    added basic endpoints for pages. changes all the hrefs. tweaked the theme dropdown menu. minor css cleaning

--- server.js
+++ server.js
@@ -11,9 +11,54 @@ app.use(express.static(__dirname + '/client/'));
     app.get('/',(req, res) => {
         // res.writeHead(200,{ 'Content-Type':'html' })
         res.status(200)
-        res.sendFile( __dirname + '/client/index.html')
+        res.sendFile( __dirname + '/index.html')
     })
-
+    app.get('/choosescreen',(req, res) => {
+        // res.writeHead(200,{ 'Content-Type':'html' })
+        res.status(200)
+        res.sendFile( __dirname + '/client/choose.html')
+    })
+    app.get('/learn',(req, res) => {
+        // res.writeHead(200,{ 'Content-Type':'html' })
+        res.status(200)
+        res.sendFile( __dirname + '/client/learn.html')
+    })
+    app.get('/fp-info',(req, res) => {
+        // res.writeHead(200,{ 'Content-Type':'html' })
+        res.status(200)
+        res.sendFile( __dirname + '/client/info.html')
+    })
+    app.get('/tp',(req, res) => {
+        // res.writeHead(200,{ 'Content-Type':'html' })
+        res.status(200)
+        res.sendFile( __dirname + '/client/two-player.html')
+    })
+    app.get('/fp',(req, res) => {
+        // res.writeHead(200,{ 'Content-Type':'html' })
+        res.status(200)
+        res.sendFile( __dirname + '/client/four-player.html')
+    })
+    app.get('/tpgameoffline',(req, res) => {
+        // res.writeHead(200,{ 'Content-Type':'html' })
+        res.status(200)
+        res.sendFile( __dirname + '/client/two-game.html')
+    })
+    app.get('/fpchoose',(req, res) => {
+        // res.writeHead(200,{ 'Content-Type':'html' })
+        res.status(200)
+        res.sendFile( __dirname + '/client/four-choose.html')
+    })
+    app.get('/fpindi',(req, res) => {
+        // res.writeHead(200,{ 'Content-Type':'html' })
+        res.status(200)
+        res.sendFile( __dirname + '/client/four-game-individual.html')
+    })
+    app.get('/fpteams',(req, res) => {
+        // res.writeHead(200,{ 'Content-Type':'html' })
+        res.status(200)
+        res.sendFile( __dirname + '/client/four-game-teams.html')
+    })
+    
 
 //! LISTEN
 app.listen(port,() => {
